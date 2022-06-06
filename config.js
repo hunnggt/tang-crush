e5d5d3496b773776f5f68ace5960c435025f636d
hunnggt <102538563+hunnggt@users.noreply.github.com>
2022-06-07 00:47:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Bé lệ có biết anh yêu bé Lệ rất nhiều không ?',
+    desc: 'Làm người yêu của anh nha bé Lệ ',
+    btnYes: 'Dồng ý <3',
+    btnNo: 'Không',
+    question:'Hứa với anh em sẽ yêu anh 1505 nha',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Hứa với em cả đời này anh sẽ chỉ yêu mình em',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/hungdubo' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
